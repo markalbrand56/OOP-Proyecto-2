aa69b49ddf4acaf807bbdc54d2367d76cd82819b
Diego2250 <mor21146@uvg.edu.gt>
2021-09-19 20:26:20 -0600

    Update vista.java

--- vista.java
+++ vista.java
@@ -11,4 +11,24 @@ public class vista{
   public vista(){
 		scan = new Scanner(System.in);
 	}
+  /*Metodo para elegír una opción
+  @return número de opción
+  */
+  public int menuOpciones(){
+    int op=0;
+        try{
+      String s = "¿Que desea hacer?\n" +
+          "1. Calcular datos de reforestación para un area\n" +
+          "2. Ver áreas registradas\n" +
+          "3. Guardar datos\n" +
+          "4. Salir";
+
+    System.out.println(s);
+    op = scan.nextInt();
+  }catch (Exception e){
+        error();
+        scan.next();
+    }
+    return op;
+  }
 }
