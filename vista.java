1b189f4425da9a1324b254452fd53cbc86326dd5
Diego2250 <mor21146@uvg.edu.gt>
2021-09-19 20:21:52 -0600

    Update vista.java

---
+++ vista.java
@@ -0,0 +1,14 @@
+/***************************************************
+Vista.java
+Autor: Diego Morales, Jimena Hernandez, Mark Albrand, Javier Azurdia
+Fecha: 19/09/2021
+
+Vista. Responsable de toda interaccion con el usuario.
+***************************************************/
+import java.util.Scanner;
+public class vista{
+  private Scanner scan;
+  public vista(){
+		scan = new Scanner(System.in);
+	}
+}
