7c1a9b66f6a28648988be834d45cb2d51a9f6b97
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2021-09-19 20:28:41 -0600

    Update Controlador.java
    Main driver and instances

--- Controlador.java
+++ Controlador.java
@@ -1,3 +1,23 @@
+/**
+ * @author: Mark Albrand, Alejandro Azurdia, Diego Morales, Jimena Hernández.
+ * @version: 0.1 
+ */
+
 public class Controlador{
     
+    int menu;
+    int area;
+    int workers;
+    int tipos_de_suelo;
+    int cantidad_de_semilla;
+    int costo;
+
+
+    //Terreno terreno; //La clase todavía no se ha creado. 
+
+
+    public static void main(String[] args) {
+        
+    }
+
 }
