b7a4c18bc6f5fba282b5b7bdf7391857fa212708
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2021-09-30 00:50:35 -0600

    BosqueLatillof

--- terrenos/BosqueLatiofolladoBE.java
+++ terrenos/BosqueLatiofolladoBE.java
@@ -1,7 +1,24 @@
 package terrenos;
+import arboles.*;
 
 public class BosqueLatiofolladoBE extends Terreno {
     public BosqueLatiofolladoBE(int area, int trabajadores){
         super(area, trabajadores);
     }
+
+    public int calc_semilla(int opcion){
+        Arbol arbolActual;
+        switch(opcion){
+            case 1:
+                arbolActual = new Encino();
+            case 2:
+                arbolActual = new Aguacatillo();
+        }
+        Double diametro = arbolActual.getDiametro();
+          //CALCULOS JIME
+         conversion = area_de_terreno/ 10000;
+         espaciofinal= (int)(conversion/diametro);
+  
+         return espaciofinal;
+     }
 }
