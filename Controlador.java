3ecd9c51a665ba6b856aacd7ea2257239ba8ad6b
Mark Albrand <mark.albrand5@gmail.com>
2021-09-29 21:32:32 -0600

    Update Controlador.java

--- Controlador.java
+++ Controlador.java
@@ -6,17 +6,8 @@
 
 public class Controlador{
     private static Vista vista = new Vista();
-    private static int menu;
-    private static int area;
-    private static int workers;
-    private static int tipos_de_suelo;
-    private static int cantidad_de_semilla;
-    private static int costo;
-
-
 
     //Terreno terreno; //La clase todavía no se ha creado. 
-
 
     public static void main(String[] args) {
         int opcion = vista.menuOpciones();
@@ -25,6 +16,8 @@ public class Controlador{
             switch (opcion) {
                 case 1:
                     // Datos de reforestación
+                    int[] dimensiones = vista.size();
+                    int trabajadores = vista.trabajadores();
                     break;
                 case 2:
                     // Ver áreas registradas
