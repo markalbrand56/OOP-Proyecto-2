b5303e001ab4c23396dfc630bbd8633b10bccbc3
Mark Albrand <mark.albrand5@gmail.com>
2021-09-30 00:52:09 -0600

    Update MonteEspinoso.java

--- terrenos/MonteEspinoso.java
+++ terrenos/MonteEspinoso.java
@@ -1,7 +1,28 @@
 package terrenos;
 
+import arboles.*;
+
 public class MonteEspinoso extends Terreno {
     public MonteEspinoso(int area, int trabajadores){
         super(area, trabajadores);
     }
+
+    public int calc_semilla(int opcion){
+        Arbol arbolActual = null;
+        switch(opcion){
+            case 1:
+                arbolActual = new Jiote();
+            case 2:
+                arbolActual = new Aripin();
+            case 3:
+                arbolActual = new Cactus();
+        }
+        Double diametro = arbolActual.getDiametro();
+          //CALCULOS JIME
+         conversion = area_de_terreno/ 10000;
+         espaciofinal= (int)(conversion/diametro);
+  
+         return espaciofinal;
+        
+   }
 }
