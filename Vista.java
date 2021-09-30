25f6a5aa89f0d1086ecf9be901117ba96f8005a1
Diego2250 <mor21146@uvg.edu.gt>
2021-09-29 20:45:00 -0600

    Update Vista.java

--- Vista.java
+++ Vista.java
@@ -8,9 +8,9 @@ Vista. Responsable de toda interaccion con el usuario.
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
-public class vista{
+public class Vista{
   private Scanner scan;
-  public vista(){
+  public Vista(){
 		scan = new Scanner(System.in);
 	}
   /*Metodo para elegír una opción
@@ -33,7 +33,54 @@ public class vista{
     return op;
   }
 
-      /**
+ /*Metodo para ingresar ancho y largo de terreno
+  @return array con ancho y largo del terreno
+  */
+  public int[] size(){
+    int[] medidas=new int[2]; 
+    try {
+      System.out.println("Ingrese el ancho del terreno");
+      int ancho=scan.nextInt(); 
+      System.out.println("Ingrese el largo del terreno");
+      int largo=scan.nextInt(); 
+      medidas[0]=ancho; 
+      medidas[1]=largo; 
+    } catch (Exception e) {
+      scan.next();
+    }
+
+    return medidas; 
+  }
+
+   /*Metodo para ingresar el tipo de terreno
+  @return tipo de suelo
+  */
+  public String TipodeSuelo(){
+    String Tipo_suelo="";
+     try {
+      System.out.println("Ingrese el tipo de terreno");
+      Tipo_suelo=scan.nextLine(); 
+     } catch (Exception e) {
+      scan.next();
+     }
+    return Tipo_suelo; 
+  }
+
+     /*Metodo para ingresar el número de trabajadores disponibles
+  @return trabajadores
+  */
+  public int trabajadores(){
+    int trabajadores=0;
+     try {
+      System.out.println("Ingrese eel número de trabajadores disponibles");
+      trabajadores=scan.nextInt(); 
+     } catch (Exception e) {
+      scan.next();
+     }
+    return trabajadores; 
+  }
+
+    /**
     * @param: String
     * imprime mensajes
     **/ 
@@ -51,6 +98,15 @@ public class vista{
       }
       
   }
+
+  public void despedida(){
+		System.out.println("Gracias por usar este programa, adios");
+	}
+	/*Metodo para mostrar mensaje de bienvenida al usuario
+	*/
+	public void bienvenida(){
+		System.out.println("Bienvendi@!");
+	}
 
 
 }
