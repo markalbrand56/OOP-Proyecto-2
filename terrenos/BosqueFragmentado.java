6c5533b6c7c40d19fb54514b6566a4cfd2787807
Mark Albrand <mark.albrand5@gmail.com>
2021-09-30 00:48:14 -0600

    Update BosqueFragmentado.java

--- terrenos/BosqueFragmentado.java
+++ terrenos/BosqueFragmentado.java
@@ -1,7 +1,28 @@
 package terrenos;
 
+import arboles.*;
+
 public class BosqueFragmentado extends Terreno {
     public BosqueFragmentado(int area, int trabajadores){
         super(area, trabajadores);
     }
+
+    public int calc_semilla(int opcion){
+        Arbol arbolActual = null;
+        switch(opcion){
+            case 1:
+                arbolActual = new Conacaste();
+            case 2:
+                arbolActual = new PaloBlanco();
+            case 3:
+                arbolActual = new CaobaSur();
+        }
+        Double diametro = arbolActual.getDiametro();
+          //CALCULOS JIME
+         conversion = area_de_terreno/ 10000;
+         espaciofinal= (int)(conversion/diametro);
+  
+         return espaciofinal;
+        
+   }
 }
