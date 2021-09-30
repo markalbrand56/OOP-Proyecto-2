f3c99142d45bb929ccc766e5c29c796a8b8ff40e
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2021-09-30 00:36:45 -0600

    calculosemilla

--- terrenos/BosqueMixto.java
+++ terrenos/BosqueMixto.java
@@ -4,20 +4,25 @@ public class BosqueMixto extends Terreno {
     public BosqueMixto(int area, int trabajadores){
         super(area, trabajadores);
     }
-    /**
-     * public calc_semilla(int opcion){
-     *      Arbol arbolActual;
-     *      switch(opcion){
-     *          case 1:
-     *              arbolActual = new MangleBlanco()
-     *          case 2:
-     *              arbolActual = new MangleBlanco()
-     *          case 3:
-     *              arbolActual = new MangleBlanco()
-     *      }
-     *      int diametro = arbolActual.getDiametro();
-     *        CALCULOS JIME
-     * }
-     * 
-     */
+    
+      public calc_semilla(int opcion){
+           Arbol arbolActual;
+           switch(opcion){
+               case 1:
+                   arbolActual = new MangleBlanco();
+               case 2:
+                   arbolActual = new MangleBlanco();
+               case 3:
+                   arbolActual = new MangleBlanco();
+           }
+           int diametro = arbolActual.getDiametro();
+             //CALCULOS JIME
+            conversion = area_de_terreno/ 10000;
+            espaciofinal= conversion/diametro;
+     
+            return espaciofinal;
+           
+      }
+      
+     
 }
