c62c0a8375141184556dcada8c28e51d32e83425
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2021-10-21 21:01:40 -0600

    menu

--- GUI.java
+++ GUI.java
@@ -7,5 +7,17 @@ public class GUI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);      
         frame.setSize(300, 300);        
         frame.setVisible(true);  
+        
+        // Creando MenuBar y agregando componentes   
+        JMenuBar mb = new JMenuBar();       
+        JMenu m1 = new JMenu("ARCHIVO");       
+        JMenu m2 = new JMenu("Ayuda");       
+        mb.add(m1);       
+        mb.add(m2);       
+        JMenuItem m11 = new JMenuItem("Abrir");       
+        JMenuItem m22 = new JMenuItem("Guardar como");       
+        m1.add(m11);       
+        m1.add(m22); 
     }
+
 }
