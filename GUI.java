352e612029c76da6f2788d3f57bf23e5a19713eb
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2021-10-21 21:15:08 -0600

    frame

--- GUI.java
+++ GUI.java
@@ -6,10 +6,10 @@ public class GUI {
     public GUI(){
         JFrame frame = new JFrame("Hello Tree");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 300);
+        frame.setSize(500, 800);
         frame.setVisible(true);
-        JLabel l = new JLabel("𓆭𓆭𓆭Semilla a semilla para mejorar el mundo𓆭𓆭𓆭 ");
-        l.setFont(new Font("Monaco", Font.PLAIN, 18));
+        JLabel l = new JLabel("Semilla a semilla para mejorar el mundo");
+        l.setFont(new Font("Monaco", Font.PLAIN, 16));
         JPanel p = new JPanel();
         p.add(l);
         frame.add(p);
