07560fb135554730999205932e8c970fa93b28c9
Mark Albrand <mark.albrand5@gmail.com>
2021-10-21 21:01:56 -0600

    Texto de prueba

--- GUI.java
+++ GUI.java
@@ -3,9 +3,13 @@ import javax.swing.*;
 public class GUI {
 
     public GUI(){
-        JFrame frame = new JFrame("Hello Tree");      
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);      
-        frame.setSize(300, 300);        
-        frame.setVisible(true);  
+        JFrame frame = new JFrame("Hello Tree");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+        JLabel l = new JLabel("Hola");
+        JPanel p = new JPanel();
+        p.add(l);
+        frame.add(p);
     }
 }
