b343637a0895d44c60cd9f0e3d28d3691fcacdc6
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2021-10-24 21:43:51 -0600

    Update Controlador.java
    Se añadieron las opciones 2 y 3, para leer y escribir en el archivo txt.

--- Controlador.java
+++ Controlador.java
@@ -4,13 +4,18 @@
  * @version: 0.1
  */
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import terrenos.*;
 
 public class Controlador{
     private static Vista vista = new Vista();
     private static Terreno terrenoActual;
     private static GUI interfaz = new GUI();
+    private static Archivos archivos = new Archivos();
 
+    private static ArrayList<String> temporal = new ArrayList<String>();
 
     /**
      * Método main del programa
@@ -74,14 +79,44 @@ public class Controlador{
                         vista.mensaje("No se pudo determinar el tiempo necesario para plantar todas las semillas.\n");
                     }
 
-                    //
+
+                    /** Orden de elementos en cadena de texto de memoria temporal del programa. 
+                     * Area del terreno
+                     * Tipo del terreno
+                     * Tipo de Árbol de terreno
+                     * Tipo de semilla del terreno
+                     * Cantidad de semillas del terreno // pendiente. 
+                     * Cantidad de mano de obra
+                     * 
+                     */
+
+                    //Guardando datos en memoria temporal del programa. 
+                    temporal.add("Area: " + terrenoActual.getAreaDeTerreno() + ". Tipo: " + terrenoActual.getTipoDeTerreno() + ". Tipo de arboles: " + terrenoActual.getArbol() + ". Cantidad de personas: " + terrenoActual.getTrabajadores()+". ");
 
                     break;
                 case 2:
-                    // Ver áreas registradas
+                    // Ver los resultados registrados. 
+                    // Se visualizarán solamente los resultados que ya han sido registrados. 
+                    ArrayList <String> mostrarArchivos = new ArrayList<String>();
+                    mostrarArchivos = archivos.leer("Resultados.txt");
+                    for(int i = 0; i<mostrarArchivos.size(); i++){
+                        vista.mensaje(mostrarArchivos.get(i));
+                    }
+
                     break;
-                case 3:
-                    // Guardar datos
+                case 3: // guardar los datos. 
+                boolean ans = false;
+                try {
+                    ans = archivos.escribir(temporal, "Resultados.txt");
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                    if(ans == true){
+                        vista.mensaje("Se han guardado los datos");
+                    }else{
+                        vista.mensaje("Ha ocurrido un error");
+                    }
                     break;
 
                 default:
